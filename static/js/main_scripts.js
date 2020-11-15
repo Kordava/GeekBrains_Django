e70eb907d1497792c4b8d57ff5045e470cdb59d0
Kordava <59284095+Kordava@users.noreply.github.com>
2020-11-15 21:32:49 +0300

    Update main_scripts.js

--- static/js/main_scripts.js
+++ static/js/main_scripts.js
@@ -1,4 +1,4 @@
-$('.tab-content').on( 'click', 'a', function(event) {
+$( document ).on( 'click', '.tab-content a', function(event) {
    if (event.target.hasAttribute('href')) {
        var link = event.target.href + 'ajax/';
        var link_array = link.split('/');
