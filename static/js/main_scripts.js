5f3c6705b00e78e695c770811ff7fb44ff8b5963
Kordava <59284095+Kordava@users.noreply.github.com>
2020-11-15 21:15:06 +0300

    Update main_scripts.js

--- static/js/main_scripts.js
+++ static/js/main_scripts.js
@@ -1,8 +1,8 @@
-$('.paginator').on( 'click', 'a', function(event) {
+$('.tab-content').on( 'click', 'a', function(event) {
    if (event.target.hasAttribute('href')) {
        var link = event.target.href + 'ajax/';
        var link_array = link.split('/');
-       if (link_array[2] == 'category') {
+       if (link_array[4] == 'category') {
            $.ajax({
                url: link,
                success: function (data) {
