e11513d2507138f3a3d17aaf6c5fdeb039acec46
Kordava <59284095+Kordava@users.noreply.github.com>
2020-11-15 22:12:36 +0300

    Update views.py

--- mainapp/views.py
+++ mainapp/views.py
@@ -199,7 +199,7 @@ def products_ajax(request, pk=None, page=1):
            }
 
            result = render_to_string(
-                        'mainapp/Templ/product_tab_content_type_main.html',
+                        'mainapp/Templ/product_tab_content_type_2.html',
                         context=content,
                         request=request)
 
