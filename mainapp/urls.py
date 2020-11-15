95463f7c9757eee62d2793072579d3aae98a1766
Kordava <59284095+Kordava@users.noreply.github.com>
2020-11-15 21:23:29 +0300

    Update urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -21,7 +21,7 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', mainapp.products, name='index'),
     path('category/<int:pk>/', mainapp.products, name='category'),
-    path('category/<int:pk>/page/<int:page>', cache_page(3600)(mainapp.products), name='page'),
+    path('category/<int:pk>/page/<int:page>', mainapp.products, name='page'),
     path('product/<int:pk>/', mainapp.product_detail, name='product'),
 ]
 
