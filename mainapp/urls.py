f515773733fb0713c4c935560399a3c33484cc8d
Kordava <59284095+Kordava@users.noreply.github.com>
2020-11-15 21:27:44 +0300

    lesson 7

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -21,7 +21,7 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', mainapp.products, name='index'),
     path('category/<int:pk>/', mainapp.products, name='category'),
-    path('category/<int:pk>/page/<int:page>', mainapp.products, name='page'),
+    path('category/<int:pk>/page/<int:page>/ajax', mainapp.products_ajax, name='page'),
     path('product/<int:pk>/', mainapp.product_detail, name='product'),
 ]
 
