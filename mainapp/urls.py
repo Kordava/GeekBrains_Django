b35b7d1793b696f64838bbe7fa7714cf2af700fe
Kordava <59284095+Kordava@users.noreply.github.com>
2020-11-15 20:43:05 +0300

    lesson 7
    ajax

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.urls import path
 import mainapp.views as mainapp
+from django.views.decorators.cache import cache_page
 
 app_name = 'mainapp'
 urlpatterns = [
     path('', mainapp.products, name='index'),
     path('category/<int:pk>/', mainapp.products, name='category'),
-    path('category/<int:pk>/page/<int:page>/', mainapp.products, name='page'),
+    path('category/<int:pk>/page/<int:page>/ajax/', cache_page(3600)(mainapp.products_ajax), name='page'),
     path('product/<int:pk>/', mainapp.product_detail, name='product'),
 ]
 
--- mainapp/views.py
+++ mainapp/views.py
@@ -13,6 +13,10 @@ import random
 from django.conf import settings
 from django.core.cache import cache
 
+from django.template.loader import render_to_string
+from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
+
 
 # Create your views here.
 
@@ -164,6 +168,42 @@ def products(request, pk=None, page=1):
 
 
     return render(request, 'mainapp/products.html', content)
+
+def products_ajax(request, pk=None, page=1):
+   if request.is_ajax():
+       links_menu = get_links_menu()
+
+       if pk:
+           if pk == '0':
+               category = {
+                   'pk': 0,
+                   'name': 'все'
+               }
+               products = get_products_orederd_by_price()
+           else:
+               category = get_category(pk)
+               products = get_products_in_category_orederd_by_price(pk)
+
+           paginator = Paginator(products, 2)
+           try:
+               products_paginator = paginator.page(page)
+           except PageNotAnInteger:
+               products_paginator = paginator.page(1)
+           except EmptyPage:
+               products_paginator = paginator.page(paginator.num_pages)
+
+           content = {
+               'links_menu': links_menu,
+               'category': category,
+               'products': products_paginator,
+           }
+
+           result = render_to_string(
+                        'mainapp/Templ/product_tab_content_type_main.html',
+                        context=content,
+                        request=request)
+
+           return JsonResponse({'result': result})
 
 def product_detail(request, pk=None):
     title = 'Подробно'
