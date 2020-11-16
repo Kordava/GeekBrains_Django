a22911f5b06012e073f84bc88bce3660aee30e66
Kordava <59284095+Kordava@users.noreply.github.com>
2020-11-16 22:53:34 +0300

    Update tests.py

--- authapp/tests.py
+++ authapp/tests.py
@@ -25,7 +25,7 @@ class TestUserManagement(TestCase):
        response = self.client.get('/')
        self.assertEqual(response.status_code, 200)
        self.assertTrue(response.context['user'].is_anonymous)
-       self.assertEqual(response.context['title'], 'главная')
+       self.assertEqual(response.context['title'], 'Главная')
        self.assertNotContains(response, 'Пользователь', status_code=200)
        # self.assertNotIn('Пользователь', response.content.decode())
 
